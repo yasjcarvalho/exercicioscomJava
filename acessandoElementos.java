e8be18090fa70154f086308706f70da4dc38105c
Yasmin Jandriele <97072307+yasjcarvalho@users.noreply.github.com>
2023-05-08 19:45:35 -0300

    Create acessandoElementos.java

---
+++ acessandoElementos.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+     int numeros[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+     String[] [] matriz = {{"00", "01"}, {"10", "11"}};
+     
+     
+
+      System.out.println("3º número " + numeros[2]);
+      System.out.println("Elemento linha 0 coluna 1: " + matriz[0] [1]);
+    }
+}
+SAÍDA: 
+3º número 7
+Elemento linha 0 coluna 1: 01
